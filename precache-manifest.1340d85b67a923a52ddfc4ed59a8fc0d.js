21e99efb3f2e2f2f1abf34d90dcfbeeedd80348a
ameyzing09 <55131226+ameyzing09@users.noreply.github.com>
2020-09-20 00:31:39 +0530

    Updates

---
+++ precache-manifest.1340d85b67a923a52ddfc4ed59a8fc0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e85156e96d97ad8e3e20f8c9d9660bda",
+    "url": "/ameykode/index.html"
+  },
+  {
+    "revision": "b4fba096e04102f7d247",
+    "url": "/ameykode/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "bf2416effa786e09cb5b",
+    "url": "/ameykode/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b4fba096e04102f7d247",
+    "url": "/ameykode/static/js/2.05ea350f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ameykode/static/js/2.05ea350f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf2416effa786e09cb5b",
+    "url": "/ameykode/static/js/main.3be80f93.chunk.js"
+  },
+  {
+    "revision": "e6437e506107dcf5af06",
+    "url": "/ameykode/static/js/runtime-main.5c2734d3.js"
+  }
+]);
